beacba6eb2a65b0740c6ef3749f85fd03361e2dc
uchiyamashouki <135842430+uchiyamashouki@users.noreply.github.com>
2026-04-16 01:14:07 +0900

    Update BattleScene.js

--- scenes/BattleScene.js
+++ scenes/BattleScene.js
@@ -1,1 +1,6 @@
+import { filterWordsByStage } from "../systems/questionSelector.js";
+import { WORDS } from "../data/words.js";
+import { gameState } from "../state/gameState.js";
 
+// ステージに応じた単語を取得
+const stageWords = filterWordsByStage(WORDS, gameState.stage);
